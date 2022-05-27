f2b442d9692d9bdf05434619a43a6924b71d5a5b
sneha2395 <agarwal.sneha.231995@gmail.com>
2022-05-27 14:53:40 +0530

    Gallery and Laurel section completed

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => (
     <SpecialMenu />
    <Chef />
     <Intro />
-   {/* <Laurels />
-    <Gallery /> */}
+   <Laurels />
+    <Gallery />
 <FindUs />
     <Footer /> 
   </div>
